f5a76ec235b993c9c0fabf1e9ffbe25de2541747
nikoshet <nnikitarrr@gmail.com>
2019-09-20 16:36:41 +0300

    added pi4j libraries for w1coordinator and w2coordinator docker containers

--- Workbench1/Workbench1_Coordinator/src/main/java/workbench1/gpio/GpioConfig.java
+++ Workbench1/Workbench1_Coordinator/src/main/java/workbench1/gpio/GpioConfig.java
@@ -6,31 +6,31 @@ import com.pi4j.io.gpio.RaspiPin;
 public class GpioConfig {
 
 
-    public static Pin W1free1pin= RaspiPin.GPIO_03;
+    public static Pin W1free1pin= RaspiPin.GPIO_23;
     public static MyGpioController W1free1_gpio = new MyGpioController(W1free1pin);
-    public static Pin W1working1pin= RaspiPin.GPIO_00;
+    public static Pin W1working1pin= RaspiPin.GPIO_24;
     public static MyGpioController W1working1_gpio = new MyGpioController(W1working1pin);
-    public static Pin  W1completed1pin= RaspiPin.GPIO_02;
+    public static Pin  W1completed1pin= RaspiPin.GPIO_25;
     public static MyGpioController W1completed1_gpio = new MyGpioController(W1completed1pin);
 
 
 
-    public static Pin W1free2pin= RaspiPin.GPIO_10;
+    public static Pin W1free2pin= RaspiPin.GPIO_26;
     public static MyGpioController W1free2_gpio = new MyGpioController(W1free2pin);
-    public static Pin W1working2pin= RaspiPin.GPIO_04;
+    public static Pin W1working2pin= RaspiPin.GPIO_27;
     public static MyGpioController W1working2_gpio = new MyGpioController(W1working2pin);
-    public static Pin W1completed2pin= RaspiPin.GPIO_06;
+    public static Pin W1completed2pin= RaspiPin.GPIO_28;
     public static MyGpioController W1completed2_gpio = new MyGpioController(W1completed2pin);
-    public static Pin W1pending2pin= RaspiPin.GPIO_05;
+    public static Pin W1pending2pin= RaspiPin.GPIO_29;
     public static MyGpioController W1pending2_gpio = new MyGpioController(W1pending2pin);
 
 
 
-    public static Pin W1free3pin= RaspiPin.GPIO_13;
+    public static Pin W1free3pin= RaspiPin.GPIO_16;
     public static MyGpioController W1free3_gpio = new MyGpioController(W1free3pin);
-    public static Pin W1working3pin= RaspiPin.GPIO_11;
+    public static Pin W1working3pin= RaspiPin.GPIO_21;
     public static MyGpioController W1working3_gpio = new MyGpioController(W1working3pin);
-    public static Pin  W1pending3pin= RaspiPin.GPIO_12;
+    public static Pin  W1pending3pin= RaspiPin.GPIO_22;
     public static MyGpioController W1pending3_gpio = new MyGpioController(W1pending3pin);
 
 
